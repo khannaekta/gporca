1093304266aee852b933388340f00e0402abdc9c
Shreedhar Hardikar <shardikar@pivotal.io>
2018-05-10 21:49:14 -0500

    Remove Hungarian notation from libnaucrates and libgpos
    Hungarian notation removal: GPOS and Naucrates libraries
    
    This is the first commit of the initiative to remove Hungarian notation
    and replace it with a new convention -- a custom convention based on
    common C++ naming conventions. The style guide for this convention is
    forthcoming. Look for an updated README with details in future commits.
    
     Co-authored-by: Melanie Plageman <mplageman@pivotal.io>
     Co-authored-by: Ekta Khanna <ekhanna@pivotal.io>
     Co-authored-by: Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
     Co-authored-by: Abhijit Subramanya <asubramanya@pivotal.io>
     Co-authored-by: Sambitesh Dash <sdash@pivotal.io>
     Co-authored-by: Omer Arap <oarap@pivotal.io>

--- libnaucrates/include/naucrates/dxl/operators/CDXLPhysicalNLJoin.h
+++ libnaucrates/include/naucrates/dxl/operators/CDXLPhysicalNLJoin.h
@@ -41,16 +41,14 @@ namespace gpdxl
 	//---------------------------------------------------------------------------
 	class CDXLPhysicalNLJoin : public CDXLPhysicalJoin
 	{
-
-		private:
-
-			// flag to indicate whether operator is an index nested loops,
-			// i.e., inner side is an index scan that uses values from outer side
-			BOOL m_fIndexNLJ;
+	private:
+		// flag to indicate whether operator is an index nested loops,
+		// i.e., inner side is an index scan that uses values from outer side
+		BOOL m_is_index_nlj;
 
 			// array holding nest params col references used for creating nestparam
 			// node during translation
-			DrgPdxlcr *m_nest_params_col_refs;
+			CDXLColRefArray *m_nest_params_col_refs;
 
 			// if nest params are required to be parsed
 			BOOL m_nest_params_exists;
@@ -60,55 +58,51 @@ namespace gpdxl
 			// private copy ctor
 			CDXLPhysicalNLJoin(const CDXLPhysicalNLJoin&);
 
-		public:
-			// ctor
-			CDXLPhysicalNLJoin(IMemoryPool *pmp, EdxlJoinType edxljt, BOOL fIndexNLJ, BOOL nest_params_exists);
-		
-			~CDXLPhysicalNLJoin();
-			
-			// accessors
-			Edxlopid Edxlop() const;
-			const CWStringConst *PstrOpName() const;
-			
-			// is operator an index nested loops?
-			BOOL FIndexNLJ() const
-			{
-				return m_fIndexNLJ;
-			}
+	public:
+		// ctor/dtor
+		CDXLPhysicalNLJoin(IMemoryPool *mp, EdxlJoinType join_type, BOOL is_index_nlj, BOOL nest_params_exists);
+
+		~CDXLPhysicalNLJoin();
+
+		// accessors
+		Edxlopid GetDXLOperator() const;
+		const CWStringConst *GetOpNameStr() const;
+
+		// is operator an index nested loops?
+		BOOL
+		IsIndexNLJ() const
+		{
+			return m_is_index_nlj;
+		}
 
 			// nest params exists for parsing
 			BOOL NestParamsExists() const;
 
 			// serialize operator in DXL format
 			virtual
-			void SerializeToDXL(CXMLSerializer *pxmlser, const CDXLNode *pdxln) const;
+			void SerializeToDXL(CXMLSerializer *xml_serializer, const CDXLNode *dxlnode) const;
 
-			void SetNestLoopParamsColRefs(DrgPdxlcr *nest_params_col_refs);
+			void SetNestLoopParamsColRefs(CDXLColRefArray *nest_params_col_refs);
 
-			DrgPdxlcr *GetNestLoopParamsColRefs() const;
+			CDXLColRefArray *GetNestLoopParamsColRefs() const;
 
-			// conversion function
-			static
-			CDXLPhysicalNLJoin *PdxlConvert
-				(
-				CDXLOperator *pdxlop
-				)
-			{
-				GPOS_ASSERT(NULL != pdxlop);
-				GPOS_ASSERT(EdxlopPhysicalNLJoin == pdxlop->Edxlop());
+    	// conversion function
+		static CDXLPhysicalNLJoin *
+		PdxlConvert(CDXLOperator *dxl_op)
+		{
+			GPOS_ASSERT(NULL != dxl_op);
+			GPOS_ASSERT(EdxlopPhysicalNLJoin == dxl_op->GetDXLOperator());
 
-				return dynamic_cast<CDXLPhysicalNLJoin*>(pdxlop);
-			}
+			return dynamic_cast<CDXLPhysicalNLJoin *>(dxl_op);
+		}
 
 #ifdef GPOS_DEBUG
-			// checks whether the operator has valid structure, i.e. number and
-			// types of child nodes
-			void AssertValid(const CDXLNode *, BOOL fValidateChildren) const;
-#endif // GPOS_DEBUG
-			
+		// checks whether the operator has valid structure, i.e. number and
+		// types of child nodes
+		void AssertValid(const CDXLNode *, BOOL validate_children) const;
+#endif  // GPOS_DEBUG
 	};
-}
-#endif // !GPDXL_CDXLPhysicalNLJoin_H
+}  // namespace gpdxl
+#endif  // !GPDXL_CDXLPhysicalNLJoin_H
 
 // EOF
-
